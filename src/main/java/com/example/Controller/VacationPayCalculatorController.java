cf5ef75dd2e300895149d559f40ef150edff83d9
Mur0riki <nevzlomaite2283@mail.ru>
2024-09-21 17:42:43 +0300

    add rest controller and application.yml

---
+++ src/main/java/com/example/Controller/VacationPayCalculatorController.java
@@ -0,0 +1,18 @@
+package com.example.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/vacationpaycalculator")
+public class VacationPayCalculatorController {
+
+
+    @GetMapping("/calculate")
+    public String getVacationPay(
+    ) {
+        return "yes, im alive";
+    }
+}
--- src/main/java/com/example/VacationPayCalculatorApplication.java
+++ src/main/java/com/example/VacationPayCalculatorApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 @EnableConfigurationProperties
 public class VacationPayCalculatorApplication {
     public static void main(String[] args) {
